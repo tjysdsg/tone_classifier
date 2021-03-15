906b9003ab15dc302a996902555f2b0c46c890e4
tjysdsg <tjy1018543509@163.com>
2021-03-15 18:15:55 +0800

    Split to train test val subsets in fix_feats_dir.py

--- fix_feats_dir.py
+++ fix_feats_dir.py
@@ -20,23 +20,32 @@ def main():
     spk2utt = []
 
     for t in TONES:
+        data = []
         for f in os.scandir(os.path.join(DATA_DIR, str(t))):
             path = f.path
-
             filename = f.name
-            tokens = filename.split('_')
-            utt, phone, start = tokens[:3]
+            data.append([filename, path])
 
-            wavscp.append(f'{filename}\t{path}\n')
-            utt2spk.append(f'{filename}\t{t}\n')
-            spk2utt.append(f'{t}\t{filename}\n')
+        from sklearn.model_selection import train_test_split
+        data_train, data_test = train_test_split(data, test_size=0.25)
+        data_train, data_val = train_test_split(data_train, test_size=0.1)
 
-    with open(os.path.join(DATA_DIR, 'wav.scp'), 'w') as f:
-        f.writelines(wavscp)
-    with open(os.path.join(DATA_DIR, 'utt2spk'), 'w') as f:
-        f.writelines(utt2spk)
-    with open(os.path.join(DATA_DIR, 'spk2utt'), 'w') as f:
-        f.writelines(spk2utt)
+        data = dict(train=data_train, test=data_test, val=data_val)
+        for subset, d in data.items():
+            for e in d:
+                filename, path = e
+                wavscp.append(f'{filename}\t{path}\n')
+                utt2spk.append(f'{filename}\t{t}\n')
+                spk2utt.append(f'{t}\t{filename}\n')
+
+            subdir = os.path.join(DATA_DIR, subset)
+            os.makedirs(subdir, exist_ok=True)
+            with open(os.path.join(subdir, 'wav.scp'), 'w') as f:
+                f.writelines(wavscp)
+            with open(os.path.join(subdir, 'utt2spk'), 'w') as f:
+                f.writelines(utt2spk)
+            with open(os.path.join(subdir, 'spk2utt'), 'w') as f:
+                f.writelines(spk2utt)
 
 
 if __name__ == '__main__':
