6142d728cfc72d2c30162c566d55321a0c9fd3b3
tjysdsg <tjy1018543509@163.com>
2021-03-16 16:24:19 +0800

    Add embedding dataset

--- train/dataset/dataset.py
+++ train/dataset/dataset.py
@@ -29,18 +29,28 @@ class SpectrogramDataset(Dataset):
 
 
 class EmbeddingDataset(Dataset):
-    def __init__(self, wav_scp, utt2label, embd_model):
-        self.wav_scp = wav_scp
-        self.utt2label = utt2label
+    def __init__(self, utt2tones: dict, embd_model):
+        self.utt2tones = utt2tones
+        self.utts = list(self.utt2tones.keys())
         self.embd_model = embd_model
 
     def __len__(self):
-        return len(self.wav_scp)
+        return len(self.utt2tones)
 
     def __getitem__(self, idx):
-        utt, filename = self.wav_scp[idx]
-        signal = np.load(filename, allow_pickle=False)
-        signal = np.moveaxis(signal, 0, 1)
-        signal = torch.from_numpy(signal.astype('float32'))
-        embd = self.embd_model(signal)
-        return signal, self.utt2label[utt]
+        from feature_extraction import get_output_path
+        utt = self.utts[idx]
+        data = self.utt2tones[utt]
+        y = []
+        x = []
+        for tone, phone, start, dur in data:
+            path = get_output_path(utt, phone, start, f'feats/{tone}')
+            signal = np.load(path, allow_pickle=False)
+            signal = np.moveaxis(signal, 0, 1)
+            signal = torch.from_numpy(signal.astype('float32'))
+            embd = self.embd_model(signal)
+            y.append(tone)
+            x.append(embd)
+        y = torch.from_numpy(np.asarray(y, dtype='int64'))
+        x = torch.from_numpy(np.asarray(x, dtype='float32'))
+        return x, y
