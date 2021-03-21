0c739891679666e8e87aeb8117d23c3472d44f57
tjysdsg <tjy1018543509@163.com>
2021-03-21 18:09:01 +0800

    Refactor data augmentation

--- train/config.py
+++ train/config.py
@@ -1,3 +1,4 @@
 NUM_CLASSES = 5
 EMBD_DIM = 128
 IN_PLANES = 16
+WAV_DIR = '/NASdata/AudioData/mandarin/AISHELL-2/iOS/data/wav/'
--- train/dataset/aug.py
+++ train/dataset/aug.py
@@ -1,23 +1,33 @@
 import random
+import os
+from typing import Tuple, List
 import librosa
 import numpy as np
 from scipy.signal import fftconvolve
 import sox
 
-noise_list_file = '/Netdata/2017/qinxy/ASV/DeepSpeaker/egs/ffsvc/data/envir/noise_wav_list'
-noise_list = []
-with open(noise_list_file) as f:
-    for line in f:
-        noise_list.append(line.replace('\n', ''))
-
-rir_list_file = '/Netdata/2017/qinxy/ASV/DeepSpeaker/egs/ffsvc/data/envir/simu_rir_list'
-rir_list = []
-with open(rir_list_file) as f:
-    for line in f:
-        rir_list.append(line.replace('\n', ''))
+ENV_WAV_LIST_DIR = '/Netdata/2017/qinxy/ASV/DeepSpeaker/egs/ffsvc/data/envir/'
 
 
-def speed_perturb(y, sr=16000):
+def get_env_wav_list(name: str) -> List[str]:
+    if name == 'noise':
+        filename = 'noise_wav_list'
+    elif name == 'music':
+        filename = 'music_wav_list'
+    elif name == 'rir':
+        filename = 'simu_rir_list'
+    else:
+        raise RuntimeError(f"Unknown environment wav list {name}")
+
+    path = os.path.join(ENV_WAV_LIST_DIR, filename)
+    ret = []
+    with open(path) as f:
+        for line in f:
+            ret.append(line.replace('\n', ''))
+    return ret
+
+
+def speed_perturb(y: np.ndarray, sr=16000) -> List[np.ndarray, np.ndarray]:
     ret = []
     for speed in [0.9, 1.1]:
         tfm = sox.Transformer()
@@ -27,14 +37,14 @@ def speed_perturb(y, sr=16000):
     return ret
 
 
-def norm_speech(y):
+def norm_speech(y: np.ndarray) -> np.ndarray:
     if np.std(y) == 0:
         return y
     y = (y - np.mean(y)) / np.std(y)
     return y
 
 
-def truncate_speech(signal, tlen, offset=None):
+def truncate_speech(signal: np.ndarray, tlen: int, offset=None) -> np.ndarray:
     if tlen is None:
         return signal
     if signal.shape[0] <= tlen:
@@ -44,31 +54,32 @@ def truncate_speech(signal, tlen, offset=None):
     return np.array(signal[offset: offset + tlen])
 
 
-def add_noise(y, noise, snr):
+def add_noise(y: np.ndarray, noise: np.ndarray, snr: float) -> np.ndarray:
     sigma_n = np.sqrt(10 ** (-snr / 10))
     return norm_speech(norm_speech(y) + norm_speech(noise) * sigma_n)
 
 
-def add_random_noise(y, snr):
-    noise, _ = librosa.load(random.choice(noise_list), sr=16000)
+def add_random_noise(y: np.ndarray, snr: float, env_wav_type='noise') -> np.ndarray:
+    noise, _ = librosa.load(random.choice(get_env_wav_list(env_wav_type)), sr=16000)
     noise = truncate_speech(noise, y.shape[0])
     return add_noise(y, noise, snr)
 
 
-def add_rir(y, rir_signal):
+def add_rir(y: np.ndarray, rir_signal: np.ndarray) -> np.ndarray:
     y = norm_speech(y)
     rir = norm_speech(rir_signal)
     return norm_speech(fftconvolve(rir, y)[0: y.shape[0]])
 
 
 def add_random_rir(y: np.ndarray) -> np.ndarray:
-    noise, _ = librosa.load(random.choice(rir_list), sr=16000)
+    noise, _ = librosa.load(random.choice(get_env_wav_list('rir')), sr=16000)
     noise = truncate_speech(noise, y.shape[0])
     return add_rir(y, noise)
 
 
-if __name__ == '__main__':
+def test():
     from sys import argv
+
     import librosa
     import soundfile as sf
 
@@ -81,3 +92,7 @@ if __name__ == '__main__':
 
     # y = add_random_rir(y)
     # sf.write(f'rir.wav', y, samplerate=16000, subtype='PCM_16')
+
+
+if __name__ == '__main__':
+    test()
